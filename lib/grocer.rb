77e5148f5df830c9afdaa025cbe373cfba51c8b6
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-30 17:46:46 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -14,7 +14,7 @@ def consolidate_cart(cart)
 reciept = []
 counter = 0
 while counter < cart.length
-if find_item_by_name_in_collection(cart[counter][:item]) = TRUE
+if find_item_by_name_in_collection(cart[counter][:item])
   
 end
 
